a3b4b7ebee07cad6019e29146582e98a42ba0fe1
suprajakothamasu <suprajakothamasu003@gmail.com>
2024-10-25 23:41:02 +0530

    first commit

---
+++ calcy.js
@@ -0,0 +1,19 @@
+function clearDisplay() {
+    document.getElementById('display').value='';
+}
+function deleteLast(){
+    const display=document.getElementById('display');
+    display.value=display.value.slice(0,-1);
+}
+function appendToDisplay(value){
+    document.getElementById('display').value+=value;
+}
+function calculate(){
+    const display=document.getElementById('display');
+    try{
+        display.value= eval(display.value);
+    }catch(error) {
+        display.value='Error';
+    }
+
+}
